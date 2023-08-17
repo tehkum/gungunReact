93f1c703373ad55e39da122302b011cab7b8fe5f
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-08-17 12:30:44 +0530

    nodemailer

--- controllers/order.js
+++ controllers/order.js
@@ -1,100 +1,244 @@
+// const Order = require("../models/orderModel");
+// const dayjs = require("dayjs");
+
+// async function getCheckoutPage(req, res) {
+//   try {
+//     res.status(200).json({
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// }
+
+// const formatDate = () => dayjs().format("YYYY-MM-DDTHH:mm:ssZ");
+
+// async function checkout(req, res) {
+//   try {
+//     const {
+//       productData,
+//       fullName,
+//       phoneNo,
+//       address1,
+//       address2,
+//       city,
+//       country,
+//       pincode,
+//       totalPrice,
+//     } = req.body;
+
+//     const order = new Order({
+//       productData: productData,
+//       fullName: fullName,
+//       phoneNo: phoneNo,
+//       address1: address1,
+//       address2: address2,
+//       city: city,
+//       country: country,
+//       pincode: pincode,
+//       totalPrice: totalPrice,
+//       orderDate: formatDate(),
+//     });
+
+//     Order.create(order)
+//       .then((createdOrder) => {
+//         // console.log('Youtube created:', createdYoutube);
+//         res.status(200).json({
+//           success: true,
+//           message: "Order added successfully",
+//         });
+//       })
+//       .catch((error) => {
+//         console.error("Error creating order:", error);
+//       });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// }
+
+// async function getDeliveryPage(req, res) {
+//   try {
+//     res.status(200).json({
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// }
+
+// async function confirmDelivery(req, res) {
+//   try {
+//     res.status(200).json({
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// }
+
+// async function getConfirmOrder(req, res) {
+//   try {
+//     res.status(200).json({
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// }
+
+// module.exports = {
+//   getCheckoutPage: getCheckoutPage,
+//   checkout: checkout,
+//   getDeliveryPage: getDeliveryPage,
+//   confirmDelivery: confirmDelivery,
+//   getConfirmOrder: getConfirmOrder,
+// };
+
 const Order = require("../models/orderModel");
 const dayjs = require("dayjs");
+const nodemailer = require("nodemailer");
+
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "gungunsewing@outlook.in",
+        pass: "gungun123",
+    },
+});
 
 async function getCheckoutPage(req, res) {
-  try {
-    res.status(200).json({
-      success: true,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    try {
+        res.status(200).json({
+            success: true,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 const formatDate = () => dayjs().format("YYYY-MM-DDTHH:mm:ssZ");
 
 async function checkout(req, res) {
-  try {
-    const {
-      productData,
-      fullName,
-      phoneNo,
-      address1,
-      address2,
-      city,
-      country,
-      pincode,
-      totalPrice,
-    } = req.body;
+    try {
+        const {
+            productData,
+            fullName,
+            phoneNo,
+            address1,
+            address2,
+            city,
+            country,
+            pincode,
+            totalPrice,
+        } = req.body;
 
-    const order = new Order({
-      productData: productData,
-      fullName: fullName,
-      phoneNo: phoneNo,
-      address1: address1,
-      address2: address2,
-      city: city,
-      country: country,
-      pincode: pincode,
-      totalPrice: totalPrice,
-      orderDate: formatDate(),
-    });
-
-    Order.create(order)
-      .then((createdOrder) => {
-        // console.log('Youtube created:', createdYoutube);
-        res.status(200).json({
-          success: true,
-          message: "Order added successfully",
+        const order = new Order({
+            productData: productData,
+            fullName: fullName,
+            phoneNo: phoneNo,
+            address1: address1,
+            address2: address2,
+            city: city,
+            country: country,
+            pincode: pincode,
+            totalPrice: totalPrice,
+            orderDate: formatDate(),
         });
-      })
-      .catch((error) => {
-        console.error("Error creating order:", error);
-      });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+
+        Order.create(order)
+            .then((createdOrder) => {
+                // console.log('Youtube created:', createdYoutube);
+
+                // Create an email message
+                const mailOptions = {
+                    from: "gungunsewing@outlook.in",
+                    to: "gungunsewing@outlook.in",
+                    subject: "New Order received",
+                    html: `
+                    <p>Hello, a new order has been placed. Here are the details:</p>
+                    <ul>
+                    <li>Product Data: ${order.productData}</li>
+                    <li>Full Name: ${order.fullName}</li>
+                    <li>Phone Number: ${order.phoneNo}</li>
+                    <li>Address Line 1: ${order.address1}</li>
+                    <li>Address Line 2: ${order.address2}</li>
+                    <li>City: ${order.city}</li>
+                    <li>Country: ${order.country}</li>
+                    <li>Pincode: ${order.pincode}</li>
+                    <li>Total Price: ${order.totalPrice}</li>
+                    <li>Order Date: ${order.orderDate}</li>
+                    </ul>
+                    <p>Thank you for your order!</p>
+                `,
+                };
+
+                // Send the email
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log("Error sending email:", error);
+                    } else {
+                        console.log("Email sent:", info.response);
+                    }
+                });
+
+                res.status(200).json({
+                    success: true,
+                    message: "Order added successfully",
+                });
+            })
+            .catch((error) => {
+                console.error("Error creating order:", error);
+            });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 async function getDeliveryPage(req, res) {
-  try {
-    res.status(200).json({
-      success: true,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    try {
+        res.status(200).json({
+            success: true,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 async function confirmDelivery(req, res) {
-  try {
-    res.status(200).json({
-      success: true,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    try {
+        res.status(200).json({
+            success: true,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 async function getConfirmOrder(req, res) {
-  try {
-    res.status(200).json({
-      success: true,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    try {
+        res.status(200).json({
+            success: true,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 module.exports = {
-  getCheckoutPage: getCheckoutPage,
-  checkout: checkout,
-  getDeliveryPage: getDeliveryPage,
-  confirmDelivery: confirmDelivery,
-  getConfirmOrder: getConfirmOrder,
+    getCheckoutPage: getCheckoutPage,
+    checkout: checkout,
+    getDeliveryPage: getDeliveryPage,
+    confirmDelivery: confirmDelivery,
+    getConfirmOrder: getConfirmOrder,
 };
