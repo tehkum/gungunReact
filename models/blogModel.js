d924ae55250d80619e7b10cf7c9a65dca7ab8aaf
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-09-03 13:39:15 +0530

    new schema

--- models/blogModel.js
+++ models/blogModel.js
@@ -25,20 +25,25 @@ const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    body: [
-      {
-        content: [
-          {
-            type: {
-              type: String,
-            },
-            content: { type: String },
-            src: { type: String },
-            alt: { type: String },
-          },
-        ],
+    body: [{
+      type: {
+        type: String,
+        required: true,
+        enum: ['image', 'subHeading', 'paragraph']
       },
-    ],
+      alt: {
+        type: String,
+        required: function() { return this.type === 'image'; }
+      },
+      src: {
+        type: String,
+        required: function() { return this.type === 'image'; }
+      },
+      content: {
+        type: String,
+        required: function() { return this.type === 'subHeading' || this.type === 'paragraph'; }
+      }
+    }],
   },
   { timeframes: true }
 );
