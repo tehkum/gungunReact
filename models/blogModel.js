71481a0d426e52033152245812ad32be6c5d3554
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-11-03 11:13:56 +0530

    blog model updated

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,52 +1,23 @@
 const mongoose = require("mongoose");
 
-// const blogSchema = new mongoose.Schema(
-//   {
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     // mainImg: String,
-//     // subtopic: [
-//     //   {
-//     //     subheading: String,
-//     //     content: String,
-//     //     image: [
-//     //       {
-//     //         type: String,
-//     //       },
-//     //     ],
-//     //   },
-//     // ],
-//   },
-//   { timeframes: true }
-// );
-
 const blogSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    mainImg: { type: String },
-    body: [
-      {
-        type: {
-          type: String,
-          required: true,
-          enum: ["image", "subHeading", "paragraph"],
-        },
-        alt: {
-          type: String,
-          // required: function() { return this.type === 'image'; }
-        },
-        src: {
-          type: String,
-          // required: function() { return this.type === 'image'; }
-        },
-        content: {
-          type: String,
-          // required: function() { return this.type === 'subHeading' || this.type === 'paragraph'; }
-        },
-      },
-    ],
+    heading: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
   { timeframes: true }
 );
