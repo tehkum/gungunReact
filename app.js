963594b3de4d6571fd07be5f0c67ba68938aa616
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-11-03 11:50:48 +0530

    cors

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ const fileUpload = require("express-fileupload");
 //   optionsSuccessStatus: 204,
 // };
 
-app.use(cors());
+// app.use(cors(corsOptions));
 
 app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/", require("./routes/main"));
